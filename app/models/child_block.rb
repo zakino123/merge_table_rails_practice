2805b5f24abae3740ada69819bb6488eb3abf035
zakino123 <15rb076@ms.dendai.ac.jp>
2022-11-15 13:16:10 +0900

    :card_file_box:child_blockテーブルを削除

--- app/models/child_block.rb
+++ app/models/child_block.rb
@@ -1,4 +1,3 @@
 class ChildBlock < ApplicationRecord
   belongs_to :team
-  has_many :parent_blocks
 end
--- app/models/parent_block.rb
+++ app/models/parent_block.rb
@@ -1,4 +1,3 @@
 class ParentBlock < ApplicationRecord
   belongs_to :team
-  belongs_to :child_block
 end
